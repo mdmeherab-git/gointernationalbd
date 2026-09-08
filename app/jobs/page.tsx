29a809b31f223ebecbf74d54a323130e019cd477
Mcl Mir <mdmeherab189@gmail.com>
2026-09-08 14:26:56 +0600

    job circular logo fix

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -589,7 +589,7 @@ export default function JobsPage() {
                 <img
                   src="/logo.svg"
                   alt="GO International BD Logo"
-                  className="h-12 w-auto cursor-pointer object-contain transition-all duration-200 hover:scale-105 hover:opacity-90 max-md:h-25"
+                  className="h-28 w-auto cursor-pointer object-contain transition-all duration-200 hover:scale-105 hover:opacity-90 max-md:h-25"
                 />
 
               </Link>
